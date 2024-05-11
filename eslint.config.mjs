73530bbf387e601801d41d7eac42a006201267aa
EdwardLinS <63514282+EdwardLinS@users.noreply.github.com>
2024-05-11 12:14:25 -0700

    Modify setup files

---
+++ eslint.config.mjs
@@ -0,0 +1,13 @@
+// eslint.config.mjs
+import js from "@eslint/js";
+
+export default [
+    js.configs.recommended,
+
+    {
+        rules: {
+            "no-unused-vars": "warn",
+            "no-undef": "warn"
+        }
+    }
+];
